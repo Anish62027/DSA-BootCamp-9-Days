fcd91ab552794911a91c284bf77d8075ebc239b7
Anish Kumar <148037890+Anish62027@users.noreply.github.com>
2024-10-15 05:22:48 -0700

    Queue.py

--- Day 2/Queue.py
+++ Day 2/Queue.py
@@ -5,35 +5,35 @@ l = [None] * size
 front = 0
 rear = -1
 
-def push(l, size, rear):
+def push(l,size,rear):
         
-    if rear == size - 1:
+    if rear==size -1:
         print("Queue Overflow")
     else:
-        a = int(input("Enter a Number: "))
-        rear += 1
-        l[rear] = a
+        a =int(input("Enter a Number :- "))
+        rear+=1
+        l[rear]=a
         print(f"Number {a} has been successfully pushed Queue")
     return rear
 
 
-def pop(l, front, rear):
-    if front > rear:
+def pop(l,front,rear):
+    if front>rear:
         print("Queue Underflow")
     else:
-        a = l[front]
-        front += 1
+        a=l[front]
+        front+=1
         print(f"Number {a} has been successfully popped Queue")
     return front
 
     
 
-def display(l, front, rear):
-    if front > rear:
+def display(l,front,rear):
+    if front>rear:
         print("Queue Underflow")
     else:
-        print("Queue elements are:")
-        for i in range(front, rear + 1):
+        print("Queue elements are :- ")
+        for i in range(front, rear+1):
             print(l[i])
 
 
