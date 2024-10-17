c03a09e98385a7f14cacc1ae05d5e211fb6a311b
Anish Kumar <148037890+Anish62027@users.noreply.github.com>
2024-10-17 19:48:51 +0530

    Add files via upload

---
+++ Day 4/reverse_li.py
@@ -0,0 +1,99 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None  
+
+class Stack:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+
+    def reverse(self):
+        prev = None
+        curr = self.tail =self.head
+        next_node = None
+
+        while(curr != None):
+            next_node = curr.next
+            curr.next = prev
+            prev = curr
+            curr = next_node
+
+        self.head = prev
+
+    
+    def is_empty(self):
+        return self.head is None
+
+    def push(self,data):
+        new_node = Node(data)
+        if self.is_empty():
+            self.head = new_node
+            return
+        else:
+    
+            new_node.next = self.head
+            self.head = new_node
+
+
+    def pop(self):
+        if self.is_empty():
+            return -1 
+        
+        top = self.head.data
+        self.head = self.head.next
+        return top
+
+    def peek(self):
+        if self.is_empty():
+            return -1
+        
+        return self.head.data
+
+
+    def display(self):
+        if self.is_empty():
+            print("Stack is UnderFlow.")
+        else:
+            temp = self.head
+            while temp!= None:
+                print(temp.data, end=" ->")
+                temp = temp.next
+            print("null")  
+
+
+stack = Stack()
+
+while True:
+    choice = int(input('''
+        Enter a Number:
+        1. Push
+        2. Pop
+        3. Peek
+        4. Display
+        5. Reverse
+        6. Exit
+
+        Enter Your Choice: '''))
+
+    if choice == 1:
+        data = int(input("Enter the value : "))
+        stack.push(data)
+        print(f"Successfully Push Elements : {data} ")
+    elif choice == 2:
+        pop = stack.pop()
+        print(f"Successfully Pop Elements : {pop} ")
+    elif choice == 3:
+        top_element = stack.peek()
+        print(f"Top Element : {top_element}")
+    elif choice == 4:
+        stack.display()
+    elif choice == 5:
+        stack.reverse()
+        print("Successfully Reverse ")
+    elif choice ==6:
+        print("Exiting the stack operations.")
+        break
+    else:
+        print("Invalid Choice, Please Enter a Number Between 1 and 5.")
