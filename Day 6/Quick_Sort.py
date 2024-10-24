4ac6fc73212fc4c7ab50e11676cf24753856ff63
Anish Kumar <148037890+Anish62027@users.noreply.github.com>
2024-10-24 02:00:08 -0700

    Quick_Sort.py

--- Day 6/Quick_Sort.py
+++ Day 6/Quick_Sort.py
@@ -7,5 +7,35 @@ def quick_sort(arr):
     right = [x for x in arr if x > pivot]
     return quick_sort(left) + middle + quick_sort(right)
 
-arr = [64, 34, 25, 12, 22, 11, 90]
-print("Quick Sort:", quick_sort(arr))
+arr = []
+
+while True:
+    choice = int(input("""
+    Menu:
+    1. Create a array
+    2. quick sort
+    3. Exit
+    Enter your choice: """))
+
+    if choice == 1:
+        arr = []
+        n = int(input("Enter the number of elements in the array: "))
+        for i in range(n):
+            elem = int(input(f"Enter element {i + 1}: "))
+            arr.append(elem)
+        print(f"Array: {arr}")
+
+    elif choice == 2:
+        if not arr:
+            print("Array is empty.")
+        else:
+            sorted_arr = quick_sort(arr)
+            print(f"Sorted array: {sorted_arr}")
+
+    elif choice == 3:
+        print("Exiting the program.")
+        break
+
+    else:
+        print("Invalid choice, please enter a number between 1 and 3.")
+
